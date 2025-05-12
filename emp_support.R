dc78c348b6a114cb22f4ef82e4aebdeea4f0c7cd
Andre Lucas <a.lucas@vu.nl>
2025-05-12 13:53:24 +0200

    adjust reading data to new datasets

--- emp_support.R
+++ emp_support.R
@@ -11,28 +11,29 @@ load_data <- function(
   ## inclusive of starting values
   if (asset_name == "EUR") {
     ## read the data
-    file_name = file.path(folder_name, "FX/EURUSD.csv")
+    file_name = file.path(folder_name, "FX/EURUSD_042025.csv")
     if (!file.exists(file_name)) throw_error()
     my_data = read.csv(file_name)
     names(my_data) = c("dates", "rate")
-    my_data$date = as.Date(my_data$dates, "%m/%d/%y")
+    my_data$date = as.Date(my_data$dates, "%m/%d/%Y")
     my_data = my_data[order(my_data$date), ]
     my_data$ret = -c(NA, 100 * diff(log(as.numeric(my_data$rate))))
-    my_data = subset(my_data, (!is.na(ret)) & (date >= "1999-01-04") & (date <= "2023-12-01"))
+    my_data = subset(my_data, (!is.na(ret)) & (date >= "1990-01-04")) 
+    plot(my_data$date, my_data$ret, type = "l")
     
     ## set the starting values
     x0 = NULL
     
   } else   if (asset_name == "RUB") {
     ## read the data
-    file_name = file.path(folder_name, "FX/USDRUB.csv")
+    file_name = file.path(folder_name, "FX/USDRUB_042025.csv")
     if (!file.exists(file_name)) throw_error()
     my_data = read.csv(file_name)
     names(my_data) = c("dates", "rate")
-    my_data$date = as.Date(my_data$dates, "%m/%d/%y")
+    my_data$date = as.Date(my_data$dates, "%m/%d/%Y")
     my_data = my_data[order(my_data$date), ]
     my_data$ret = c(NA, 100 * diff(log(as.numeric(my_data$rate))))
-    my_data = subset(my_data, (!is.na(ret)) & (date >= "1996-01-02") & (date <= "2023-09-06"))
+    my_data = subset(my_data, (!is.na(ret)) & (date >= "1996-01-02")) 
     
     ## set the starting values
     x0 = NULL
