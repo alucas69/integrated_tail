b9671590c5e3b373181841938bf1ef508b6fd692
Andre Lucas <a.lucas@vu.nl>
2025-08-12 14:27:28 +0200

    augment functionality for MSE and Nolde Ziegel comparisons

--- PZC_simulation.R
+++ PZC_simulation.R
@@ -61,7 +61,7 @@ s_EVT_OPTIONS = list(
 #######################################################
 ## STUDENT T WITH SCALE 1 AND TV NU
 generate_data_student_t_v1 <- function(
-    dim_n, alphas, df_min = 2.5, df_max = 15, nr_cycles = 3, 
+    dim_n, alphas, df_min = 2.5, df_max = 10, nr_cycles = 3, 
     set_seed = NULL) {
 
   VaRtrue = function(sigma, df_inv, alpha) {
@@ -91,7 +91,7 @@ generate_data_student_t_v1 <- function(
 ################
 ## Generate data
 ################
-dim_n = 100000
+dim_n = 200000
 my_data = generate_data_student_t_v1(dim_n, alphas_extreme, 
                                      nr_cycles = 3,
                                      set_seed = s_PZC_OPTIONS$set_seed)
@@ -132,28 +132,64 @@ my_data = EVT_optimizer_outputs$my_data; EVT_optimizer_outputs$my_data = NULL
 EVT_PZC_plot(my_data, min(alphas_extreme), sub_idx = 75e3:80e3)
 
 
+###############################################
+## print the result
+###############################################
+print("MSE Results")
+NZ_MSE = function(x, r1, r2, r1true, r2true, idx = NULL, alpha, scale = 1e3) {
+  # for alpha close to 1 and right tail VaR !!
+  if (is.null(idx)) idx = 1:length(x)
+  ee = data.frame(
+    MAE_VaR = abs(r1 - r1true),
+    MSE_VaR = (r1 - r1true)^2,
+    NZ_2.19 = scale * ( (1 - alpha) * r1 + ifelse(x > r1, x - r1, 0) ),
+    NZ_2.20 = scale * ( (1 - alpha) * log(r1) + ifelse(x > r1, log(x/r1), 0) ),
+    MAE_ES = abs(r2 - r2true),
+    MSE_ES = (r2 - r2true)^2,
+    NZ_2.23 = scale * ( (x > r1) * 0.5 * (x - r1) / sqrt(r2) + (1 - alpha) * 0.5 * (r1 + r2) / sqrt(r2) ),
+    NZ_2.24 = scale * ( (x > r1) * (x - r1) / r2 + (1 - alpha) * (r1 / r2 - 1 + log(r2)) )
+  )
+  return(ee)
+}
+out1 = NZ_MSE(-my_data$y, 
+              -my_data$EVT_VaR_alpha0.001, -my_data$EVT_EL_alpha0.001,
+              -my_data$VaRtrue_alpha0.001, -my_data$ELtrue_alpha0.001, 
+              idx = NULL, 1 - min(alphas_extreme))
+out2 = NZ_MSE(-my_data$y, 
+              -my_data$PZC_VaR0Neld_alpha0.001, -my_data$PZC_EL0Neld_alpha0.001,
+              -my_data$VaRtrue_alpha0.001, -my_data$ELtrue_alpha0.001, 
+              idx = NULL, 1 - min(alphas_extreme))
+out_tab = rbind( colMeans(out1), colMeans(out2) )
+out_tab = rbind(out_tab, out_tab)
+for (i1 in 1:ncol(out_tab)) {
+  aid1 = t.test(out1[ , i1] - out2[ , i1])
+  out_tab[3, i1] = aid1$statistic
+  out_tab[4, i1] = aid1$p.value
+}
+rownames(out_tab) = c('EVT', 'PZC', 'DM', 'DM pval')
+print(round(out_tab,3))
+
+
 
 ###############################################
 ## subselect figure data in separate dataframe
 ###############################################
-store_filename = paste0("simulation_PZC_EVT.csv")
+store_filename = paste0("simulation_PZC_EVT_200k.csv")
 save_PZC_EVT_frame(my_data, store_filename = store_filename,
                    select_names = c(
                      'dates', 'y', 'df_inv', 'VaRtrue_alpha0.05',
-                     'VaRtrue_alpha0.001', 'ELtrue_alpha0.001', 
-                     'PZC_VaR0Neld_alpha0.001', 'PZC_EL0Neld_alpha0.001', 
-                     'EVT_tau', 
-                     # 'EVT_ft', 'EVT_bandL', 'EVT_bandU', 
+                     'VaRtrue_alpha0.001', 'ELtrue_alpha0.001',
+                     'PZC_VaR0Neld_alpha0.001', 'PZC_EL0Neld_alpha0.001',
+                     'EVT_tau',
+                     # 'EVT_ft', 'EVT_bandL', 'EVT_bandU',
                      'EVT_VaR_alpha0.001', 'EVT_EL_alpha0.001',
                      NULL),
                    new_names = c(
                      'dates', 'y', 'df_inv', 'tau_true',
-                     'VaRtrue_alpha0.001', 'ELtrue_alpha0.001', 
-                     'PZC_VaR_alpha0.001', 'PZC_EL_alpha0.001', 
-                     'EVT_tau', 
-                     # 'EVT_ft', 'EVT_bandL', 'EVT_bandU', 
+                     'VaRtrue_alpha0.001', 'ELtrue_alpha0.001',
+                     'PZC_VaR_alpha0.001', 'PZC_EL_alpha0.001',
+                     'EVT_tau',
+                     # 'EVT_ft', 'EVT_bandL', 'EVT_bandU',
                      'EVT_VaR_alpha0.001', 'EVT_EL_alpha0.001',
                      NULL))
-
-
 
