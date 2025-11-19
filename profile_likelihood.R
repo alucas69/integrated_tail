4d0aa052f5b10f01b6417765d8f9fc4d5706488f
Andre Lucas <a.lucas@vu.nl>
2025-11-19 12:44:08 +0100

    add additional plots to simulation

--- profile_likelihood.R
+++ profile_likelihood.R
@@ -1,6 +1,7 @@
 ###################
 ## Set up functions
 ###################
+library(latex2exp)
 library(ggplot2)
 library(ggpubr)
 library(Rcpp)
@@ -11,6 +12,9 @@ library(doRNG)
 library(utils)
 nr_free_cores = max(1, detectCores() - 1)
 registerDoParallel(cores = nr_free_cores)
+
+sourceCpp('tail_llik.cpp')
+
 
 
 tail_llik = function(POT_data, omega, alpha, f1) {
@@ -24,23 +28,26 @@ tail_llik = function(POT_data, omega, alpha, f1) {
 }
 
 
+# set.seed(1234)
 
 dim_T = 5000
 f1_true = 0.5
 omega_true = 1.5e-5
 alpha_true = 0.01
-dim_S = 1000
+dim_S = 100
 
 POT_data = f_true = rep(0, dim_T)
 if (!exists('df_sim')) df_sim = data.frame()
 
 # compute grid 
-dim_gr = 100
-alpha_grid = 0.04 * (1:dim_gr - 1)/(dim_gr - 1)
-logomega_grid = -5 - 15 * (1:dim_gr - 1)/(dim_gr - 1)
+dim_gr = 50
+m_alpha = 1
+m_omega = 8
+alpha_grid = 0.02 * (1:(m_alpha*dim_gr) - 1)/(m_alpha*dim_gr - 1)
+logomega_grid = -7.5 - 12.5 * (1:(m_omega*dim_gr) - 1)/(m_omega*dim_gr - 1)
 gr_points = data.frame(
-  logomega = rep(logomega_grid, each = dim_gr),
-  alpha = rep(alpha_grid, dim_gr),
+  logomega = rep(logomega_grid, each = length(alpha_grid)),
+  alpha = rep(alpha_grid, length(logomega_grid)),
   llik = 0
 )
 
@@ -61,8 +68,20 @@ for (i0 in 1:dim_S) {
   # compute likelihood over grid and pick optimum
   f1_hat = mean(log(1+POT_data[1:50]))
   gr_points$llik = foreach(i1=1:nrow(gr_points), .combine = c) %dopar% {
-    tail_llik(POT_data, exp(gr_points[i1, 'logomega']), 
+    tail_llik_cpp(POT_data, exp(gr_points[i1, 'logomega']), 
               gr_points[i1, 'alpha'], f1_hat)
+  }
+  if (dim_S == 1) {
+    # make plot and stop
+    aid1 = gr_points[which.max(gr_points$llik), ]
+    gr_points = subset(gr_points, alpha == aid1$alpha[1])
+    gg1 = ggplot(data = gr_points, aes(y=llik, x=logomega)) + geom_line() +
+      ylab('profile log-lik') + xlab('log(omega)') + ggtitle(paste0('T=',10*dim_T)) +
+      geom_vline(xintercept = log(omega_true), color='red', linetype = 2) +
+      geom_vline(xintercept = aid1$logomega, color='blue', linetype = 2)
+    plot(gg1)
+    # ggsave('simulation5000.2.png')
+    stop('intermediate stop to save plot')
   }
 
   aid1 = gr_points[which.max(gr_points$llik),]
@@ -70,7 +89,7 @@ for (i0 in 1:dim_S) {
   df_sim[i0, paste0('logomega(T=',10*dim_T,')')] = aid1[1, 'logomega']
   df_sim[i0, paste0('llik(T=',10*dim_T,')')] = aid1[1, 'llik']
   
-  if (i0 %% 100 == 0) {
+  if (i0 %% 10 == 0) {
     nr_bins = ceiling(dim_S^0.5)
     df_plot = data.frame(
       alpha = df_sim[,paste0('alpha(T=', 10*dim_T, ')')],
@@ -83,27 +102,43 @@ for (i0 in 1:dim_S) {
     gg = list(3); gg[[1]]=gg1; gg[[2]]=gg2; gg[[3]]=gg3; 
     plot(ggarrange(plotlist=gg,ncol=2,nrow=2))
     
-    print(paste0('T=', 10*dim_T, '; % at lower bound: ', 100 * length(which(df_plot[,'logomega'] < min(df_plot[,'logomega'] + 0.1)))/nrow(df_plot), '%'))
+    cat(paste0('\nT=', 10*dim_T, '; % at lower bound: ', 100 * length(which(df_plot[,'logomega'] < min(df_plot[,'logomega'] + 0.1)))/nrow(df_plot), '%'))
   }
 }
 
-nr_bins = ceiling(dim_S^0.5)
 df_plot = data.frame(
   alpha = df_sim[,paste0('alpha(T=', 10*dim_T, ')')],
   logomega = df_sim[,paste0('logomega(T=', 10*dim_T, ')')],
   llik = df_sim[,paste0('llik(T=', 10*dim_T, ')')]
 )
+df_plot = df_plot[complete.cases(df_plot), ]
+dim_S = nrow(df_plot)
+nr_bins = ceiling(dim_S^0.5)
 gg1 = ggplot(data = df_plot, aes(x=logomega)) + geom_histogram(aes(y=after_stat(density)), bins=nr_bins) + geom_density()
 gg2 = ggplot(data = df_plot, aes(x=alpha)) + geom_histogram(aes(y=after_stat(density)), bins=nr_bins) + geom_density()
 gg3 = ggplot(data = df_plot, aes(x=alpha, y=logomega)) + geom_jitter()
 gg = list(3); gg[[1]]=gg1; gg[[2]]=gg2; gg[[3]]=gg3; 
 plot(ggarrange(plotlist=gg,ncol=2,nrow=2))
 
-print(paste0('T=', 10*dim_T, '; % at lower bound: ', 100 * length(which(df_plot[,'logomega'] < min(df_plot[,'logomega'] + 0.1)))/nrow(df_plot), '%'))
+cat(paste0('\nT=', 10*dim_T, '; % at lower bound: ', 100 * length(which(df_plot[,'logomega'] < min(df_plot[,'logomega'] + 0.1)))/nrow(df_plot), '%'))
 
-save_file = 'profile_results.Rds'
-if (file.exists(save_file)) {
-  answer = menu(c('Yes','No'), title = paste0('"', save_file, '" exists: overwrite?'))
-  if (answer == 1) save(df_sim, file = save_file)
-}
+# save_file = 'profile_results.Rds'
+# if (file.exists(save_file)) {
+#   answer = menu(c('Yes','No'), title = paste0('"', save_file, '" exists: overwrite?'))
+#   if (answer == 1) save(df_sim, file = save_file)
+# }
 
+# df_plot$logomega = -df_plot$logomega
+# gg1 = ggplot(data = df_plot, aes(x = logomega)) + 
+#   geom_histogram(aes(y = after_stat(density)), bins = nr_bins, color = 'lightgray', fill = 'lightgray') + 
+#   geom_density(color = 'blue') + 
+#   geom_vline(xintercept = -log(1.5e-5)) +
+#   theme_bw() +   # (i) white background
+#   labs(title = "T=500,000", x = TeX("$-\\log(\\omega)$"), y = "Density") +   # (iii) title and labels
+#   theme(
+#     axis.title.x = element_text(size = 14),   # (ii) larger x label
+#     axis.title.y = element_text(size = 14),   # (ii) larger y label
+#     axis.text = element_text(size=14),
+#     plot.title = element_text(size = 16, face = "bold")  # (iii) larger title
+#   )
+# plot(gg1)
